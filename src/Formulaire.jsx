cc19313366e5e86bd8e6a8b909289f13ffa18abe
tayfun <tayfun.altuntas@epitech.eu>
2020-07-16 10:37:46 +0200

    Ajout des State

--- src/Formulaire.jsx
+++ src/Formulaire.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormControl, TextField, Button } from '@material-ui/core';
 
 function Formulaire() {
+    const [prenom, setPrenom] = useState("");
+    const [nom, setNom] = useState("");
+    const [adresse, setAdresse] = useState("");
+
     return (
         <FormControl>
-            <TextField id="standard-basic" label="prenom" />
-            <TextField id="standard-basic" label="nom" />
-            <TextField id="standard-basic" label="adresse" />
+            <TextField id="standard-basic" label="prenom" inputProps={prenom} onChange={(element) => {setPrenom(element)}}/>
+            <TextField id="standard-basic" label="nom" inputProps={nom} onChange={(element) => { setNom(element) }}/>
+            <TextField id="standard-basic" label="adresse" inputProps={adresse} onChange={(element) => { setAdresse(element) }}/>
             <Button>Valider</Button>
         </FormControl>
 
